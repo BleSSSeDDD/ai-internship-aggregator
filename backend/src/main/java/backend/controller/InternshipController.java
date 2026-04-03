04253ec723ddd98e3692d459174410594d72025e
YoungJEFR <artemey11@mail.ru>
2026-04-03 21:28:51 +0300

    Жесткий и рисковый, всегда соска как младенец

--- backend/src/main/java/backend/controller/InternshipController.java
+++ backend/src/main/java/backend/controller/InternshipController.java
@@ -10,10 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Set;
 
-@CrossOrigin(origins = {
-        "http://localhost:5173",
-        "http://localhost:3000"
-})@RestController
+@RestController
 @RequestMapping("/api/internship/")
 @RequiredArgsConstructor
 public class InternshipController {
